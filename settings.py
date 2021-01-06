0e3b9512517639ba3ce2ce955688a446ebaa1bd1
blumen <samsonovzastrelilsa@gmail.com>
2021-01-06 14:32:28 +0700

    added right version

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ class Settings():
 		self.screen_width=1200
 		self.screen_height=800
 		self.bg_color=(230,230,230)
-		self.bullet_width=500
+		self.bullet_width=5
 		self.bullet_height=15
 		self.bullet_color=(60,60,60)
 		self.bullets_allowed=3
